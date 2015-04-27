103b2601dcb9f63ab54ffcbfa3c7fcc807215c54
Matt James <matuse@gmail.com>
2015-04-27 13:13:47 -0600

    Updated units
    fixed a math problem and expanded error checking.

--- units.py
+++ units.py
@@ -32,21 +32,40 @@ example usage:
 
 def ft_m(number=1, units='ft'):
     '''converts between feet and meters'''
-    if units == 'ft':
-        return number / .3048
+    if number < 0:
+        return ValueError('number must be non-negative')
 
-    return number / 3.28084
+    if units == 'ft':
+        return number * .3048
+
+    if units == 'm':
+        return number * 3.28084
+
+    return ValueError('not valid units')
 
 def lbs_kg(number=1, units='lbs'):
     '''converts between lbs and kg'''
-    if units == 'lbs':
-        return number / .453592
+    if number < 0:
+        return ValueError('number must be non-negative')
 
-    return number / 2.20462
+    if units == 'lbs':
+        return number * .453592
+
+    if units == 'kg':
+        return number * 2.20462
+
+    return ValueError('not valid units')
 
 if __name__ == '__main__':
     print( '10 feet to meters' )
     print( ft_m(10, 'ft') )
     print( '10 lbs to kg' )
     print( lbs_kg(10, 'lbs') )
-    
+    print( '10 meters to feet' )
+    print( ft_m(10, 'm') )
+    print( '10 kg to lbs' )
+    print( lbs_kg(10, 'kg') )
+    print( '10 ton to kg' )
+    print( lbs_kg(10, 'ton') )
+    print( '-10 ton to kg' )
+    print( lbs_kg(-10, 'ton') )
